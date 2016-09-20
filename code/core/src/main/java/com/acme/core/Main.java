92017e77bc857e5270d6e0a6f4760274f7c7c071
Cedric Champeau <cedric.champeau@gmail.com>
2016-09-20 19:00:40 +0200

    Fix extra Main class

--- code/core/src/main/java/com/acme/core/Main.java
+++
@@ -1,8 +0,0 @@
-package com.acme.core;
-import com.acme.utils.GreeterUtils;
-
-public class Main {
-    public static void main(String... args) {
-        System.out.println(GreeterUtils.defaultGreeting() + "javaOne");
-    }
-}
